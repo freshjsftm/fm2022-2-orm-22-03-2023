76709ccc60d9442ca287faf6262f4f76fa03ec9c
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-22 12:03:22 +0200

    Add server, app, router

---
+++ router.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const router = Router();
+
+//method & controllers
+//http://localhost:3000/api/test
+router.get('/test', ()=>{})
+
+module.exports = router;
