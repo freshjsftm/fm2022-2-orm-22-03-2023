98bf60edad84aaf5c9dfe7afd242b446653b7ba7
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-24 09:54:06 +0200

    Add create user

---
+++ controllers/user.controller.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const createdUser = await User.create(body);
+    console.log(createdUser);
+    res.status(201).send(createdUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// module.exports.createUser = async (req, res, next) => {
+//   try {
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- router.js
+++ router.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-
+const UserController = require('./controllers/user.controller');
 const router = Router();
 
 //method & controllers
 //http://localhost:3000/api/test
-router.get('/test', ()=>{})
+//router.get('/test', ()=>{})
+
+//users
+//http://localhost:3000/api
+router.post('/users', UserController.createUser);
 
 module.exports = router;
